c797f1ff0ae7045637493b94aa1f32032cb53940
ramer <ramer-mobile@yandex.ru>
2017-07-10 07:09:08 +0300

    updated version info

--- IPromptTestVB/My Project/AssemblyInfo.vb
+++ IPromptTestVB/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' используя "*", как показано ниже:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.0.1.0")>
+<Assembly: AssemblyFileVersion("1.0.1.0")>
